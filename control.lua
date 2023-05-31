1a063b0ef7fd4006e89bb151c8a7aa5cd0da671d
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-05-30 23:59:32 -0700

    target the player when they exit a spidertron

--- control.lua
+++ control.lua
@@ -298,6 +298,23 @@ local function on_spider_command_completed(event)
   end
 end
 
+---@param event EventData.on_player_driving_changed_state
+local function on_player_driving_changed_state(event)
+  local spider = event.entity
+  if not spider then return end
+  global.ignored_spidertrons = global.ignored_spidertrons or {}
+  if global.ignored_spidertrons[spider.name] then return end
+  local player = game.get_player(event.player_index)
+  if not player then return end
+  if not (spider.type == "spider-vehicle") then return end
+  if player.vehicle then return end
+  if spider.get_driver() then return end
+  if spider.get_passenger() then return end
+  local character = player.character
+  if not character then return end
+  spider.follow_target = character
+end
+
 ---@param spidertron LuaEntity
 local function add_spider(spidertron)
   global.ignored_spidertrons = global.ignored_spidertrons or {}
