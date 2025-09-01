482ed91c0bb537f2aaa56af988ed655ac3fe7457
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2025-09-01 12:50:52 -0700

    removed nudge when no valid wander target is found

--- control.lua
+++ control.lua
@@ -266,7 +266,7 @@ local function send_spider_wandering(spidertron)
         else
             storage.try_again_next_tick[unit_number] = nil
             chatty_print(chatty_name .. " did not find a wander target. giving up")
-            nudge_spidertron(spidertron)
+            -- nudge_spidertron(spidertron)
         end
         return
     else
