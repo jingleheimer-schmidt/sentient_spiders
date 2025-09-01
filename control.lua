de0c8d4335bd7400197dc1adb19e4517b0f285a4
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2025-09-01 12:51:00 -0700

    reworked nudge behavior to search for tiles the player doesn't collide with

--- control.lua
+++ control.lua
@@ -207,7 +207,7 @@ local function nudge_spidertron(spidertron)
         local non_colliding_position = spidertron.surface.find_tiles_filtered {
             position = nearby_position,
             radius = 10,
-            collision_mask = { "water_tile" },
+            collision_mask = { "player" },
             invert = true,
             limit = 1,
         }
